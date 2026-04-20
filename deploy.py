0300f69d99c4b0ddbb43c0f2c30334f46650f1b0
sonawaneaakanksha2503-dotcom <sonawane.aakanksha2503@gmail.com>
2026-04-20 10:46:58 +0530

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,5 +1,24 @@
 import boto3
+import os
 
-s3 = boto3.client('s3')
+# Get credentials from GitHub Secrets
+AWS_ACCESS_KEY = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 
-s3.upload_file('index.html', 'automate-file-upload-on-s3', 'index.html')
+BUCKET_NAME = "automate-file-upload-on-s3"
+
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=AWS_ACCESS_KEY,
+    aws_secret_access_key=AWS_SECRET_KEY
+)
+
+# Upload index.html
+s3.upload_file(
+    'index.html',
+    BUCKET_NAME,
+    'index.html',
+    ExtraArgs={'ContentType': 'text/html'}
+)
+
+print("✅ Website uploaded successfully to S3!")
